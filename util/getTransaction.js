e90396039db93f2eca77679c31cca2dbc3baa71b
brandonbryant12 <brandon.bryant002@gmail.com>
2022-03-16 16:03:31 -0400

    fix get Transaction

--- util/getTransaction.js
+++ util/getTransaction.js
@@ -1,6 +1,6 @@
 const http = require('superagent');
 async function getTransaction (txid) {
-    const url = `https://api.whatsonchain.com/v1/bsv/main/tx/77b0544730bed6492d91658eda78340b7dea25f5bb332fb5a8979040b3599170/hex`
+    const url = `https://api.whatsonchain.com/v1/bsv/main/tx/${txid}/hex`
     let tx  = (await http.get(url)).text;
     return tx;
 }
